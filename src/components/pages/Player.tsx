1ffee7eea121692959001aaaf6e791d74dd8626d
bmalkes <bmalkes@gmail.com>
2023-07-01 21:00:34 +0300

    feat:add structure base UI

--- src/components/pages/Player.tsx
+++ src/components/pages/Player.tsx
@@ -1,7 +1,24 @@
+import { MessageCircle } from "lucide-react";
 const Player = () => {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
-      <div className="flex w-[110px] flex-col gap-6"></div>
+      <div className="flex w-[1100px] flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-2xl font-bold">Fundamnetos do redux</h1>
+            <span className="text-sm text-zinc-400">
+              Modulos desvedando o redux
+            </span>
+          </div>
+          <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
+            <MessageCircle className="w-4 h-4" /> deixar FeedBack
+          </button>
+        </div>
+        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
+          <div className="flex-1">video</div>
+          <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[900px]"></aside>
+        </main>
+      </div>
     </div>
   );
 };
